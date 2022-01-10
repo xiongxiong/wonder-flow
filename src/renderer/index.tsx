616a140eabe62335e448b944bcb7da4072dc34a7
wangjirong <wangjirong@newstrength.com>
2022-01-10 14:01:22 +0800

    update

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,6 +2,9 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/store';
+import {enableMapSet} from "immer";
+
+enableMapSet()
 
 render(
   <Provider store={store}>
